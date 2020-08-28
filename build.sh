53ddccc66661f492c4c1ed986362b58aa74c8d73
jimmywangking <51218306+jimmywangking@users.noreply.github.com>
2020-08-28 18:51:57 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mvn clean package -U
+
+docker build -t registry.cn-hangzhou.aliyuncs.com/springcloud-baron/eureka .
+
+docker push registry.cn-hangzhou.aliyuncs.com/springcloud-baron/eureka
+
